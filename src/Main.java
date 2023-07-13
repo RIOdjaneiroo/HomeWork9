6d0296855b9c0ca273609d5c2a985abc2401de42
Ivor Rudenko <rudenko23@gmail.com>
2023-07-13 23:10:50 +0300

    refaktoring 3

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import java.util.Collection;
 
 public class Main {
     public static void main(String[] args) {
         //System.out.printf("Hello and welcome!");
-        myArrayListTest();
+        myLinkedListTest();
+        //myArrayListTest();
 
+    }
+    public static void myLinkedListTest(){
+        MyLinkedList myLinkedList=new MyLinkedList<>();
+        myLinkedList.add("wertar");
+        myLinkedList.add("wont");
+        myLinkedList.add("sleep");
+        for (int i = 0; i< myLinkedList.size(); i++) {
+            System.out.println(myLinkedList.get(i));
+        }
+        myLinkedList.remove(1);
+        System.out.println("=================================================");
+        for (int i = 0; i< myLinkedList.size(); i++) {
+            System.out.println(myLinkedList.get(i));
+        }
+        System.out.println("myLinkedList.size() = " + myLinkedList.size());
+        myLinkedList.clear();
+        System.out.println("myLinkedList.size() = " + myLinkedList.size());
     }
     public static void myArrayListTest(){
         MyArrayList myArrayList = new MyArrayList<>();
@@ -14,7 +33,7 @@ public class Main {
         for(int i = 0; i< myArrayList.size(); i++){
             System.out.println("s = " + myArrayList.get(i));
         }
-        System.out.println("myArrayList.get(1) = " + myArrayList.get(1));
+        System.out.println("myArrayList.get( 1) = " + myArrayList.get(1));
         System.out.println("myArrayList.size() = " + myArrayList.size());
         myArrayList.remove(1);
         System.out.println("myArrayList.get(1) = " + myArrayList.get(1));
--- src/MyLinkedList.java
+++ src/MyLinkedList.java
@@ -1,2 +1,76 @@
-public class MyLinkedList {
+public class MyLinkedList<T> {
+    private Node<T> firstNode;
+    private Node<T> lastNode;
+    private int size;
+
+    public MyLinkedList() { // конструктор
+        firstNode = null;
+        lastNode = null;
+        size = 0;
+    }
+
+    public void add(T val) {
+        Node<T> newNode = new Node<>(lastNode, null, val);      // створюємо нову ноду
+        if (lastNode != null) {                       // якщо посилання на останній елемент не ноль
+            lastNode.setNextNode(newNode);            // встановлюємо посилання на нову створену ноду
+        }
+        lastNode = newNode;                      // в іншому випадку посилання на останню ноду буде невНода
+        if (firstNode == null) {                 // якщо посилання на попередній елемент =0 то
+            firstNode = newNode;                 // встановлюємо посилання на новуНоду
+        }
+        size++;                                  // збільшуємо розмір ЛінкедЛіста
+    }
+
+    public void remove(int index) {  //метод видалення за індексом
+        IndOutOfBoundExn(index);             //перевіряємо чи індекс не виходить за межі колекції
+        Node<T> nodeToRemove = getNode(index);   // оголошуємо ноду, що видаляється
+        Node<T> previousNode = nodeToRemove.getPrevNode();// беремо посилання на попередню ноду
+        Node<T> nextNode = nodeToRemove.getNextNode();    // беремо посилання на наступну ноду
+
+        if (previousNode != null) {                      // якщо посилання на попередню нода не = нулю тобто елемент не перший
+            previousNode.setNextNode(nextNode);          //встановлюємо ій посилання на наступну ноду визначену з ноди що видаляється
+        } else {
+            firstNode = nextNode;                        // якщо посилання на попередню ноду = нулю то встановлюємо першій ноді значення наступної
+        }
+
+        if (nextNode != null) {                          //якщо наступна нада не = 0 тобто елемент не оснанній
+            nextNode.setPrevNode(previousNode);          //їй встановлюється посилання на попередню ноду, посилання що містить нода яка видаляється
+        } else {                                         // якщо ж елемент останній тобто наступна нада = 0
+            lastNode = previousNode;                     // останній ноді встановлюємо значення попередньої
+        }
+
+        size--;                                          // зменшуємо розмір колекції
+    }
+
+    public void clear() {                             // очищаємо колекцію
+        firstNode = null;
+        lastNode = null;
+        size = 0;
+    }
+
+    public int size() {                                // визначаємо розмір
+        return size;
+    }
+
+    public T get(int index) {                           // метод виведення по індексу
+        IndOutOfBoundExn(index);                        // перевіряємо чи входить індекс в масив
+        Node<T> node = getNode(index);                  // присвоюємо обєкту  індксне значення
+        return node.getValue();                         // виводимо значення елементу за індексом
+    }
+
+    private void IndOutOfBoundExn(int index) {            // метод що викликає вийняток для індекса що не входить в колекцію
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+    }
+
+    private Node<T> getNode(int index) {                 // метод що шукає ноду за вказаним індексом
+        IndOutOfBoundExn(index);                         // перевіряємо чи індекс в межах колекції
+        Node<T> currentNode = firstNode;                 // створюємо змінну з типом нода і присвоюємоїй перший елемент
+        for (int i = 0; i < index; i++) {                // рухаємося по масиву до вказаного індексу -1
+            currentNode = currentNode.getNextNode();     // getNextNode() повертає посилання на наступний вузол після поточного вузла.
+        }
+        return currentNode;                              //повертає знайдений вузол за вказаним індексом
+    }
 }
+
